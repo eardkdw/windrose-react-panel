464671c9d53c42ebc5195d33e3b3c4899adb0c84
Dan Walker <daniel.walker@ncas.ac.uk>
2021-11-25 11:36:28 +0000

    Initial import of test panel

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+type SeriesSize = 'sm' | 'md' | 'lg';
+type Palette = 'reds' | 'greens' | 'blues';
+
+export interface SimpleOptions {
+  text: string;
+  showSeriesCount: boolean;
+  seriesCountSize: SeriesSize;
+  palette: Palette;
+}
