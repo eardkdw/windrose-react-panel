6535bb8b8e7e375051854422b608c3f296965dde
Dan Walker <daniel.walker@ncas.ac.uk>
2021-12-02 11:02:13 +0000

    Remove redundant attributes

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,7 @@
-type SeriesSize = 'sm' | 'md' | 'lg';
 type Palette = 'reds' | 'greens' | 'blues';
 type SpeedUnit = 'ms¯¹' | 'kmh¯¹' | 'mph' | 'kts';
 
 export interface SimpleOptions {
-  text: string;
-  showSeriesCount: boolean;
-  seriesCountSize: SeriesSize;
   palette: Palette;
   numberOfSegments: number;
   windSpeedInterval: number;
